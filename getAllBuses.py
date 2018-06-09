89616a948c48b57a74fc6fda0e95d8482a172aaa
tomershmul <32766936+tomershmul@users.noreply.github.com>
2018-06-09 18:24:07 +0300

    Add files via upload

--- getAllBuses.py
+++ getAllBuses.py
@@ -8,7 +8,7 @@ from busColorParameters import *
 import shutil
 
 
-def get_bus(image_box, bus_color, imageName, test_flag):
+def get_bus(image_box, bus_color, imageName, test_flag, aug_flag):
 #    image_box.show()
     color_dict = {'1': 'Green', '2': 'Yellow', '3': 'White', '4': 'Grey', '5': 'Blue', '6': 'Red'}
     busImageName = os.path.basename(str(imageName)).split('.')[0] + '_' + color_dict[str(bus_color)] + str(get_bus.counter[bus_color]) + ".jpg"
@@ -18,9 +18,11 @@ def get_bus(image_box, bus_color, imageName, test_flag):
 
 #    if(get_bus.counter[bus_color] % 5 == 0):
     if(test_flag==1):
-        image_box_path = os.path.abspath(os.path.join(cropedBusesFolderPath, 'test'))
+        image_box_path = testCropedBusesFolder
+    elif (aug_flag==0):
+        image_box_path = trainCropedBusesFolder
     else:
-        image_box_path = os.path.abspath(os.path.join(cropedBusesFolderPath, 'train'))
+        image_box_path = augImagesFolder
 
 #    image_box_path = os.path.abspath(os.path.join(trainBusesFolder, 'train'))
     pathlib.Path(image_box_path).mkdir(parents=True, exist_ok=True)
@@ -37,6 +39,8 @@ if os.path.exists(testCropedBusesFolder):
 get_bus.counter = [0, 1, 1, 1, 1, 1, 1] #first element is for easy reading
 
 annotationsOrigFile = open(annotationsOrigFilePath,"r")
+
+#augImagesFolder = os.path.abspath(os.path.join(trainCropedBusesFolder, 'aug_img'))
 
 for line in annotationsOrigFile:    #Read File Line by Line
     #Get image
@@ -57,9 +61,13 @@ for line in annotationsOrigFile:    #Read File Line by Line
     
     if(type(listObjects) is tuple):             #Handle images with MORE than 1 bus
         for i in listObjects:  
+            color=i[4]
             if test_flag:                       #do not change crop size for test images
+                aug_flag=0
                 im_box=image.crop((i[0], i[1], i[0] + i[2], i[1] + i[3]))
+                get_bus(im_box, color, imageName, test_flag, aug_flag)
             else:
+                aug_flag=1
                 crop_delta_x=crop_factor*i[2]
                 crop_delta_y=crop_factor*i[3]
                 xmin=max(0,i[0] - crop_delta_x)
@@ -67,15 +75,23 @@ for line in annotationsOrigFile:    #Read File Line by Line
                 xmax=min(imageSize[0], (i[0] + i[2])+crop_delta_x)
                 ymax=min(imageSize[1], (i[1] + i[3])+crop_delta_y)
                 im_box=image.crop((xmin, ymin, xmax, ymax))
+                get_bus(im_box, color, imageName, test_flag, aug_flag)
+                
+                aug_flag=0
+                im_box=image.crop((i[0], i[1], i[0] + i[2], i[1] + i[3]))
+                get_bus(im_box, color, imageName, test_flag, aug_flag)
             #xmin=i[0] ; ymin=i[1] ; xmax=xmin + i[2] ; ymax=ymin + i[3]
 #            im_box=image.crop((i[0], i[1], i[0] + i[2], i[1] + i[3]))
 #            im_box=image.crop((xmin, ymin, xmax, ymax))
-            color=i[4]
-            get_bus(im_box, color, imageName, test_flag)
+#            get_bus(im_box, color, imageName, test_flag, aug_flag)
     if(type(listObjects) is list):             #Handle images with 1 bus
+        color=listObjects[4]
         if test_flag:                          #do not change crop size for test images
+            aug_flag=0
             im_box=image.crop((listObjects[0], listObjects[1], listObjects[0] + listObjects[2], listObjects[1] + listObjects[3]))
+            get_bus(im_box, color, imageName, test_flag, aug_flag)
         else:
+            aug_flag=1
             crop_delta_x=crop_factor*listObjects[2]
             crop_delta_y=crop_factor*listObjects[3]
             xmin=max(0,listObjects[0] - crop_delta_x)
@@ -83,11 +99,15 @@ for line in annotationsOrigFile:    #Read File Line by Line
             xmax=min(imageSize[0], (listObjects[0] + listObjects[2])+crop_delta_x)
             ymax=min(imageSize[1], (listObjects[1] + listObjects[3])+crop_delta_y)
             im_box=image.crop((xmin, ymin, xmax, ymax))
+            get_bus(im_box, color, imageName, test_flag, aug_flag)
+            
+            aug_flag=0
+            im_box=image.crop((listObjects[0], listObjects[1], listObjects[0] + listObjects[2], listObjects[1] + listObjects[3]))
+            get_bus(im_box, color, imageName, test_flag, aug_flag)
         #xmin=listObjects[0] ; ymin=listObjects[1] ; xmax=xmin + listObjects[2] ; ymax=ymin + listObjects[3]
 #        im_box=image.crop((listObjects[0], listObjects[1], listObjects[0] + listObjects[2], listObjects[1] + listObjects[3]))
 #        im_box=image.crop((xmin, ymin, xmax, ymax))
-        color=listObjects[4]
-        get_bus(im_box, color, imageName, test_flag)
+#        get_bus(im_box, color, imageName, test_flag, aug_flag)
                
 annotationsOrigFile.close()
 
@@ -95,24 +115,24 @@ annotationsOrigFile.close()
 
 num_of_Aug_images = 100
 
-p = Augmentor.Pipeline(trainCropedBusesFolder)
-p.rotate90(probability=0.5)
-p.rotate270(probability=0.5)
-p.flip_left_right(probability=0.8)
-p.flip_top_bottom(probability=0.3)
+p = Augmentor.Pipeline(augImagesFolder)
+#p.rotate90(probability=0.5)
+#p.rotate270(probability=0.5)
+#p.flip_left_right(probability=0.8)
+#p.flip_top_bottom(probability=0.3)
 p.crop_random(probability=1, percentage_area=0.8)
 p.sample(num_of_Aug_images)
 
-augImagesFolder = os.path.abspath(os.path.join(trainCropedBusesFolder, 'output'))
-augImagesFolder = os.path.join(augImagesFolder, '')
+augmentorImagesFolder = os.path.abspath(os.path.join(augImagesFolder, 'output'))
+augmentorImagesFolder = os.path.join(augmentorImagesFolder, '')
 
 aug_idx=0
-for aug_img in os.listdir(augImagesFolder):
-    print(aug_img)
-    aug_img_name = re.search('train_original_(.*)\.jpg_', aug_img, re.IGNORECASE).group(1)
+for aug_img in os.listdir(augmentorImagesFolder):
+#    print(aug_img)
+    aug_img_name = re.search('aug_img_original_(.*)\.jpg_', aug_img, re.IGNORECASE).group(1)
     aug_img_name = aug_img_name + '_augmentated_' + str(aug_idx)
-    print(aug_img_name)
-    os.rename(augImagesFolder + str(aug_img), trainCropedBusesFolder + str(aug_img_name) + ".jpg")
+#    print(aug_img_name)
+    os.rename(augmentorImagesFolder + str(aug_img), trainCropedBusesFolder + str(aug_img_name) + ".jpg")
     aug_idx += 1
 
 if os.path.exists(augImagesFolder):    
