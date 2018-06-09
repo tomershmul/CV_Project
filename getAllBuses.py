de4e5e0b0198c6cba5062dd570d9917094cd04ce
tomershmul <32766936+tomershmul@users.noreply.github.com>
2018-06-09 20:54:33 +0300

    Add files via upload

--- getAllBuses.py
+++ getAllBuses.py
@@ -101,16 +101,15 @@ annotationsOrigFile.close()
 
 #image Augmentation
 
-num_of_Aug_images = 200
+num_of_Aug_images = 500
 
 p = Augmentor.Pipeline(augImagesFolder)
-#p.rotate90(probability=0.5)
-#p.rotate270(probability=0.5)
-#p.flip_left_right(probability=0.8)
-#p.flip_top_bottom(probability=0.3)
 p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
 p.crop_random(probability=1, percentage_area=0.85)
+p.random_brightness(probability=0.6, min_factor=0.5, max_factor=1.5)
 p.sample(num_of_Aug_images)
+
+
 
 augmentorImagesFolder = os.path.abspath(os.path.join(augImagesFolder, 'output'))
 augmentorImagesFolder = os.path.join(augmentorImagesFolder, '')
