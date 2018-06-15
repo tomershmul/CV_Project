5e1785fbaa6d4536a509b4fa2a95aebf851e80e9
tomershmul <32766936+tomershmul@users.noreply.github.com>
2018-06-15 10:13:11 +0300

    Add files via upload

--- getAllBuses.py
+++ getAllBuses.py
@@ -6,10 +6,12 @@ import re
 import Augmentor
 from busColorParameters import *
 import shutil
+import random
+from shutil import copy
 
 
 def get_bus(image_box, bus_color, imageName, test_flag, aug_flag):
-    color_dict = {'1': 'Green', '2': 'Yellow', '3': 'White', '4': 'Grey', '5': 'Blue', '6': 'Red'}
+#    color_dict = {'1': 'Green', '2': 'Yellow', '3': 'White', '4': 'Grey', '5': 'Blue', '6': 'Red'}
     busImageName = os.path.basename(str(imageName)).split('.')[0] + '_' + color_dict[str(bus_color)] + str(get_bus.counter[bus_color]) + ".jpg"
 
     if(test_flag==1):
@@ -19,30 +21,65 @@ def get_bus(image_box, bus_color, imageName, test_flag, aug_flag):
     else:
         image_box_path = augImagesFolder
 
-#    image_box_path = os.path.abspath(os.path.join(trainBusesFolder, 'train'))
     pathlib.Path(image_box_path).mkdir(parents=True, exist_ok=True)
     image_box_path = os.path.abspath(os.path.join(image_box_path, busImageName))
     image_box.save(image_box_path)
     get_bus.counter[bus_color] += 1
     
-#Remove folders from Previous run
+#YOLO train and test Folders
+testYoloFolder = os.path.abspath(os.path.join(rootDir, 'test', 'Images'))
+testYoloAnnotationsFolder = os.path.abspath(os.path.join(rootDir, 'test', 'Annotations'))
+trainYoloFolder = os.path.abspath(os.path.join(rootDir, 'train', 'Images'))
+trainYoloAnnotationsFolder = os.path.abspath(os.path.join(rootDir, 'train', 'Annotations'))
+
+#Remove folders from previous run
+if os.path.exists(testYoloFolder):    
+    shutil.rmtree(testYoloFolder, ignore_errors=False, onerror=None)
+if os.path.exists(testYoloAnnotationsFolder):    
+    shutil.rmtree(testYoloAnnotationsFolder, ignore_errors=False, onerror=None)
+if os.path.exists(trainYoloFolder):    
+    shutil.rmtree(trainYoloFolder, ignore_errors=False, onerror=None)
+if os.path.exists(trainYoloAnnotationsFolder):    
+    shutil.rmtree(trainYoloAnnotationsFolder, ignore_errors=False, onerror=None)
+
 if os.path.exists(trainCropedBusesFolder):    
     shutil.rmtree(trainCropedBusesFolder, ignore_errors=False, onerror=None)
 if os.path.exists(testCropedBusesFolder):    
     shutil.rmtree(testCropedBusesFolder, ignore_errors=False, onerror=None)
+    
+#Make Dir for YOLO train and test
+pathlib.Path(testYoloFolder).mkdir(parents=True, exist_ok=True)
+pathlib.Path(testYoloAnnotationsFolder).mkdir(parents=True, exist_ok=True)
+pathlib.Path(trainYoloFolder).mkdir(parents=True, exist_ok=True)
+pathlib.Path(trainYoloAnnotationsFolder).mkdir(parents=True, exist_ok=True)
 
 get_bus.counter = [0, 1, 1, 1, 1, 1, 1] #first element is for easy reading
 
 annotationsOrigFile = open(annotationsOrigFilePath,"r")
 
+#Choose Random Images for Test Set
+num_of_Images=sum(1 for _ in annotationsOrigFile)
+num_of_TestImages=int(0.1*num_of_Images)
+testLines = random.sample(range(num_of_Images),num_of_TestImages)
+annotationsOrigFile.close()
+
+annotationsOrigFile = open(annotationsOrigFilePath,"r")
+line_idx=0
+
 for line in annotationsOrigFile:    #Read File Line by Line
     #Get image
     imageName = line.split(":")[0]
-    if re.match('DSCF106', imageName):
+    imagePath = os.path.abspath(os.path.join(allImagesFolder, imageName))
+    annotationPath = os.path.abspath(os.path.join(allAnnotationsFolder, imageName.split('.')[0]) + '.xml')
+#    if re.match('DSCF106', imageName):
+    if line_idx in testLines:
         test_flag=1
+        copy(imagePath, testYoloFolder)
+        copy(annotationPath, testYoloAnnotationsFolder)
     else:
         test_flag=0
-    imagePath = os.path.abspath(os.path.join(allImagesFolder, imageName))
+        copy(imagePath, trainYoloFolder)
+        copy(annotationPath, trainYoloAnnotationsFolder)
     image=Image.open(imagePath)
 #    image.show()
     imageObjects = line.split(":")[1]
@@ -50,7 +87,7 @@ for line in annotationsOrigFile:    #Read File Line by Line
     #Handle Objects Parameters
     listObjects=eval(imageObjects.split()[0])   #Split into list of lists
     
-    crop_factor=0.01
+#    crop_factor=0.01
     
     if(type(listObjects) is tuple):             #Handle images with MORE than 1 bus
         for i in listObjects:  
@@ -96,12 +133,14 @@ for line in annotationsOrigFile:    #Read File Line by Line
             im_box=image.crop((listObjects[0], listObjects[1], listObjects[0] + listObjects[2], listObjects[1] + listObjects[3]))
             #xmin=listObjects[0] ; ymin=listObjects[1] ; xmax=xmin + listObjects[2] ; ymax=ymin + listObjects[3]
             get_bus(im_box, color, imageName, test_flag, aug_flag)
-                     
+            
+    line_idx+=1
+    
 annotationsOrigFile.close()
 
 #image Augmentation
 
-num_of_Aug_images = 800
+#num_of_Aug_images = 800
 
 p = Augmentor.Pipeline(augImagesFolder)
 p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
